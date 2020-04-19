cf51dee7738e25fec1747b386748725fb5221167
Conrad Pankoff <deoxxa@fknsrs.biz>
2020-04-19 19:59:15 +1000

    add text drawing routines to display code

--- display.h
+++ display.h
@@ -9,6 +9,13 @@ typedef struct display_s {
   void (*set_pixel)(struct display_s* d, int x, int y, char v);
   void (*flush)(struct display_s* d);
   int flip;
+  int x, y;
 } display_t;
 
+void display_init(display_t* d);
 void display_flip(display_t* d, int flip);
+void display_move(display_t* d, int x, int y);
+void display_putc_5x8(display_t* d, char ch);
+void display_puts_5x8(display_t* d, const char* s);
+void display_putc_3x5(display_t* d, char ch);
+void display_puts_3x5(display_t* d, const char* s);
